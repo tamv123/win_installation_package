5ff3787443910de0f3457cc0cdff49fd0ded9cc3
Victor Tam <75426520+tamv123@users.noreply.github.com>
2021-03-09 15:24:04 +0800

    Update install.bat with batterybar
    Add batterybar for installation

--- install.bat
+++ install.bat
@@ -2,6 +2,6 @@
 title This is your first batch script!
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 echo Chocolatey installed!
-choco install -y anaconda3 knime notepadplusplus virtualbox win32diskimager powerbi windirstat 7zip git docker vscode putty sharpkeys
-echo Installed: anaconda3 knime notepadplusplus virtualbox win32diskimager powerbi windirstat 7zip git docker vscode putty sharpkeys
+choco install -y anaconda3 knime notepadplusplus virtualbox win32diskimager powerbi windirstat 7zip git docker vscode putty sharpkeys batterybar
+echo Installed: anaconda3 knime notepadplusplus virtualbox win32diskimager powerbi windirstat 7zip git docker vscode putty sharpkeys batterybar
 pause
